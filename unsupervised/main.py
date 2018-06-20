9598fd37690408e602e9fe996407fd43121ee13e
rmparanhos <raffaelmp@id.uff.br>
2018-06-19 22:04:52 -0300

    Arquivos pra versao nao supervisionada

--- unsupervised/main.py
+++ unsupervised/main.py
@@ -1,10 +1,6 @@
 import codecs
 import pandas as pd
-
-class tagN:
-    def __init__(self, nome, qtd):
-         self.nome = nome
-         self.qtf = qtd
+from sklearn.cluster import KMeans
 
 uta = pd.read_table("../data/user_taggedartists-timestamps.dat", sep="\t", header=0, names=["userID", "artistsID", "tagID","timestamp"])
 uta = uta.drop(["timestamp"], axis=1)
@@ -22,6 +18,7 @@ utajoin = pd.merge(uta,tags,on=["tagID","tagID"])
 utajoin2 = pd.merge(utajoin,artists,on=["artistsID","artistsID"])
 
 utajoin2 = utajoin2.drop(["userID","tagID","artistsID"],axis=1)
+
 
 def achatags(artista):
     listatags = []
@@ -45,12 +42,38 @@ def achatags(artista):
             qtd.remove(maior)
             listatags.remove(maior_tag)
 
+    return listatagsfinal
 
-    return(listatagsfinal)
 
-def produzDataFrame():
+utaAgrupado = utajoin2.groupby(["name", "value"]).size().reset_index(name='count')
+col = ['Artista','Tag1','Tag2','Tag3','Tag4','Tag5','Tag6','Tag7','Tag8','Tag9','Tag10']
+
+
+def produzdf():
+    resp = pd.DataFrame(columns=col)
+    i = 1
     for index, row in artists.iterrows():
-        print(row["name"])
-        print(achatags(row["name"]))
+        print(i)
+        utaagrupado2 = utaAgrupado[utaAgrupado["name"] == row["name"]].sort_values(by=['count'], ascending=[False])
+        lista_tags = utaagrupado2["value"].tolist()
+        lista_tags = lista_tags[:10]
+        lista_tags.insert(0, row["name"])
+        #lista_tags_df = pd.DataFrame([lista_tags])
+        if len(lista_tags) == 11:
+            resp.loc[len(resp)] = lista_tags
+        #resp = resp.append(lista_tags_df, ignore_index=True)  # ignoring index is optional
+        i = i + 1
+    resp.to_csv("../unsupervised/data/lista_tags_virgula.csv", sep=',')
 
-produzDataFrame()
+
+def kmeans_lista_tags():
+    lista_tags = pd.read_csv("../unsupervised/data/lista_tags_virgula.csv", sep=',')
+    lista_tags = lista_tags.drop(["ID"], axis=1)
+    lt_array = lista_tags.values
+    print(lt_array)
+    kmeans = KMeans(n_clusters=10, random_state=0).fit(lt_array)
+    print(kmeans)
+
+
+#produzdf()
+kmeans_lista_tags()
